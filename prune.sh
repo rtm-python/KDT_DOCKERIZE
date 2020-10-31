8f0e075dc429f0422167b8e4ecba330bf82c1fcc
root <root@localhost.localdomain>
2020-11-01 01:17:53 +0600

    Minor fixes

--- prune.sh
+++ prune.sh
@@ -2,4 +2,5 @@
 
 yes | docker container prune
 yes | docker image prune
-docker image rm --force kdt_dockerize_api kdt_dockerize_neo4j
+docker image rm --force kdt_dockerize_api kdt_dockerize_neo4j kdt_dockerize_elastic
+docker image rm --force csv2neo4j_api:devel csv2neo4j_neo4j:devel csv2neo4j_elastic:devel
